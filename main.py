d428baf2849cd094909e6d4909a58a05322133b5
BannieKatarina <katarina.burmistrova@yandex.ru>
2025-04-13 23:13:51 +0400

    Переделано движение игрока для читаемости кода

--- main.py
+++ main.py
@@ -6,29 +6,16 @@ pygame.init()
 screen = pygame.display.set_mode((1200, 800))
 player = pygame.rect.Rect(580, 380, 40, 40)
 clock = pygame.time.Clock()
+actons = {pygame.K_w: [1, -1], pygame.K_s: [1, 1], pygame.K_a: [0, -1], pygame.K_d: [0, 1]}
 motion = [0, 0]
 speed = 5
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                motion[1] -= 1
-            if event.key == pygame.K_s:
-                motion[1] += 1
-            if event.key == pygame.K_a:
-                motion[0] -= 1
-            if event.key == pygame.K_d:
-                motion[0] += 1
+            motion[actons[event.key][0]] += actons[event.key][1]
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w:
-                motion[1] += 1
-            if event.key == pygame.K_s:
-                motion[1] -= 1
-            if event.key == pygame.K_a:
-                motion[0] += 1
-            if event.key == pygame.K_d:
-                motion[0] -= 1
+            motion[actons[event.key][0]] -= actons[event.key][1]
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
