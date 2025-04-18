43c5a147064700920f31c64761e30efadfb82b43
BannieKatarina <katarina.burmistrova@yandex.ru>
2025-04-18 12:29:18 +0400

    Сделан класс игрока

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pygame
 import sys
 
+class Player():
+    def __init__(self, x, y):
+        self.player = pygame.rect.Rect(x, y, 100, 100)
+        self.speed = 8
+    def update(self):
+        pygame.draw.rect(screen, (255, 0, 0), self.player)
+    def move(self, motion):
+        self.player = self.player.move(motion[0] * self.speed, motion[1] * self.speed)
+
 pygame.init()
 
 screen = pygame.display.set_mode((1200, 800))
-player = pygame.rect.Rect(580, 380, 40, 40)
+player = Player(550, 350)
 clock = pygame.time.Clock()
 actons = {pygame.K_w: [1, -1], pygame.K_s: [1, 1], pygame.K_a: [0, -1], pygame.K_d: [0, 1]}
 motion = [0, 0]
@@ -12,15 +21,15 @@ speed = 5
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and event.key in actons:
             motion[actons[event.key][0]] += actons[event.key][1]
-        if event.type == pygame.KEYUP:
+        if event.type == pygame.KEYUP and event.key in actons:
             motion[actons[event.key][0]] -= actons[event.key][1]
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     screen.fill((0, 35, 30))
-    player = player.move(motion[0] * speed, motion[1] * speed)
-    pygame.draw.rect(screen, (225, 0, 0), player)
+    player.move(motion)
+    player.update()
     pygame.display.update()
     clock.tick(60)
